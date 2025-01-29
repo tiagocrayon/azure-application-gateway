ee874045572e675f8f1e8db1a29f50cbffe93b8a
Tiago Isabelinho <tiago.isabelinho@crayon.com>
2025-01-29 11:20:49 +0000

    america add backend_settings

--- terraform/environments/beta/backend_settings.tf
+++ terraform/environments/beta/backend_settings.tf
@@ -10,36 +10,88 @@ variable "backend_settings" {
   }))
   default = [
     {
-      name                    = "Https-settings-443"
-      cookie_based_affinity   = "Disabled"
-      port                    = 443
-      protocol                = "Https"
-      request_timeout         = 20
+      name: "Http-settings-80",
+      cookie_based_affinity: "Disabled",
+      port: 80,
+      protocol: "Http",
+      request_timeout: 20
     },
     {
-      name                    = "Http-settings-80"
-      cookie_based_affinity   = "Disabled"
-      port                    = 80
-      protocol                = "Http"
-      request_timeout         = 20
+      name: "Http-settings-8112",
+      cookie_based_affinity: "Disabled",
+      port: 8112,
+      protocol: "Http",
+      request_timeout: 20
+    }
+    {
+      name: "Http-settings-9090",
+      cookie_based_affinity: "Disabled",
+      port: 9090,
+      protocol: "Http",
+      request_timeout: 20
     },
     {
-      name                    = "Http-settings-8112"
-      cookie_based_affinity   = "Disabled"
-      port                    = 8112
-      protocol                = "Http"
-      request_timeout         = 20
-      pick_host_name_from_backend_address = true
-      probe_name              = "youtrack-health-probe"
+      name: "Http-settings-9190",
+      cookie_based_affinity: "Disabled",
+      port: 9190,
+      protocol: "Http",
+      request_timeout: 20
     },
     {
-      name                    = "Http-settings-default"
-      cookie_based_affinity   = "Disabled"
-      port                    = 8113
-      protocol                = "Http"
-      request_timeout         = 20
-      pick_host_name_from_backend_address = true
-      probe_name              = "default-health-probe"
+      name: "Http-settings-9290",
+      cookie_based_affinity: "Disabled",
+      port: 9290,
+      protocol: "Http",
+      request_timeout: 20
+    },
+    {
+      name: "Http-settings-9291",
+      cookie_based_affinity: "Disabled",
+      port: 9291,
+      protocol: "Http",
+      request_timeout: 20
+    },
+    {
+      name: "Http-settings-9292",
+      cookie_based_affinity: "Disabled",
+      port: 9292,
+      protocol: "Http",
+      request_timeout: 20
+    },
+    {
+      name: "Http-settings-9490",
+      cookie_based_affinity: "Disabled",
+      port: 9490,
+      protocol: "Http",
+      request_timeout: 20
+    },
+    {
+      name: "Http-settings-9192",
+      cookie_based_affinity: "Disabled",
+      port: 9192,
+      protocol: "Http",
+      request_timeout: 20
+    },
+    {
+      name: "Http-settings-9193",
+      cookie_based_affinity: "Disabled",
+      port: 9193,
+      protocol: "Http",
+      request_timeout: 20
+    },
+    {
+      name: "Http-settings-9194",
+      cookie_based_affinity: "Disabled",
+      port: 9194,
+      protocol: "Http",
+      request_timeout: 20
+    },
+    {
+      name: "Http-settings-9590",
+      cookie_based_affinity: "Disabled",
+      port: 9590,
+      protocol: "Http",
+      request_timeout: 20
     }
   ]
 }
