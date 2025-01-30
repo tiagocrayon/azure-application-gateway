27c70c9c6a086bfeccfb4083036411072ecdc8f1
Tiago Isabelinho <tiago.isabelinho@crayon.com>
2025-01-30 09:38:41 +0000

    world: add backend-setting and health probes

--- terraform/environments/world/probe.tf
+++ terraform/environments/world/probe.tf
@@ -28,6 +28,62 @@ variable "probe" {
         status_code = ["200"]
         body        = ""
       }
+    },
+    {
+      name                                      = "http-81-health-probe"
+      pick_host_name_from_backend_http_settings = true
+      protocol                                  = "Http"
+      port                                      = 81
+      path                                      = "/health"
+      interval                                  = 30
+      timeout                                   = 20
+      unhealthy_threshold                       = 3
+      match = {
+        status_code = ["200"]
+        body        = ""
+      }
+    },
+    {
+      name                                      = "http-82-health-probe"
+      pick_host_name_from_backend_http_settings = true
+      protocol                                  = "Http"
+      port                                      = 82
+      path                                      = "/health"
+      interval                                  = 30
+      timeout                                   = 20
+      unhealthy_threshold                       = 3
+      match = {
+        status_code = ["200"]
+        body        = ""
+      }
+    },
+    {
+      name                                      = "healthz-http-9192-health-probe"
+      pick_host_name_from_backend_http_settings = true
+      protocol                                  = "Http"
+      port                                      = 9192
+      path                                      = "/healthz"
+      interval                                  = 30
+      timeout                                   = 20
+      unhealthy_threshold                       = 3
+      match = {
+        status_code = ["200"]
+        body        = ""
+      }
+    },
+    {
+      name                                      = "swagger-index-http-9290-health-probe"
+      pick_host_name_from_backend_http_settings = true
+      protocol                                  = "Http"
+      port                                      = 9290
+      path                                      = "/swagger/index.html"
+      interval                                  = 30
+      timeout                                   = 20
+      unhealthy_threshold                       = 3
+      match = {
+        status_code = ["200"]
+        body        = ""
+      }
     }
   ]
 }
