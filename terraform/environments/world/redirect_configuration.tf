39ab32cf68e3bfcb396cda089d8bf39aa331e2eb
Tiago Isabelinho <tiago.isabelinho@crayon.com>
2025-01-30 12:04:20 +0000

    world: add redirect-configuration

--- terraform/environments/world/redirect_configuration.tf
+++ terraform/environments/world/redirect_configuration.tf
@@ -7,7 +7,19 @@ variable "redirect_configuration" {
     include_query_string  = bool
   }))
   default = [
-    
+    {
+      name                  = "redirect-360imprimir-pt"
+      redirect_type         = "Permanent"
+      target_listener_name  = "360imprimir_pt-listener-https-443"
+      include_path          = true
+      include_query_string  = true
+    },
+    {
+      name                  = "redirect-360print-pt"
+      redirect_type         = "Permanent"
+      target_listener_name  = "360print_pt-listener-https-443"
+      include_path          = true
+      include_query_string  = true
+    },
   ]
 }
-
