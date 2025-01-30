181659d32094bdfb0217103c04723b50177aa367
Tiago Isabelinho <tiago.isabelinho@crayon.com>
2025-01-30 09:41:03 +0000

    world: add frontend_port

--- terraform/environments/world/frontend_port.tf
+++ terraform/environments/world/frontend_port.tf
@@ -7,6 +7,22 @@ variable "frontend_port" {
     {
       name = "port-80"
       port = 80
+    },
+    {
+      name = "port-443"
+      port = 443
+    },
+    {
+      name = "port-81"
+      port = 81
+    },
+    {
+      name = "port-82"
+      port = 82
+    },
+    {
+      name = "port-9290"
+      port = 9290
     }
   ]
 }
